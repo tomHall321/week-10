1b3a7d58673d6672e3c8b60aaf6649517c34be57
tom <tomhall4321@gmail.com>
2020-11-30 15:55:39 +0000

    more elegant version

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -6,66 +6,46 @@ class SignUp extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			passwords: [
-				{password: ''},
-				{password: ''},
-			],
-			match: false,
+            password: "",
+            confirm: "",
 		}
-		this.update = this.update.bind(this);
+
 		this.validate = this.validate.bind(this);
 
 	}
 
-	update(e, index) {
-        const passwordObjectsArray = {...this.state.passwords};
-        //spread operator to obtain a list of password objects from the array
-        passwordObjectsArray[index].password = e.target.value;
-        //whichever password object is being update is the target
-        this.setState({passwords: passwordObjectsArray});
-        //updates it 
-        this.validate();
-        //then calls validate method to check if they are the same
-
-	}
-
 	validate() {
-        //need both password object to be the same and also the length to be longer than or equal to 4
-		if(this.state.passwords[0].password === this.state.passwords[1].password && this.state.passwords[0].password.length >= 4 ) {
-			this.setState({match: true})
-		} else {
-			this.setState({match: false})
-		}
+        let {password, confirm} = this.state;
+        return password === confirm && password.length >= this.props.minimumLength  
+        
 	}
 
-  render() {
-  	return (
-  		<form>
-  			<Password 
-                label="Password" 
-                index={0}
-                //this is the 0 indexed object in the array started with this.state
-  				value={this.state.passwords[0].password} 
-  				onChange={(e, index) => {
-	  					this.update(e, index);
-	  				}
-  				} 
-  				match={this.state.match}
-  			/>
-  			<Password 
-                label="Confirm password" 
-                index={1}
-                //this is the 1 indexed object in the array started with this.state
-  				value={this.state.passwords[1].password} 
-  				onChange={(e, index) => {
-	  					this.update(e, index);
-	  				}
-  				}  
-  				match={this.state.match}
-  			/>
-  		</form>
-  	);
-  }
+    render() {
+        let valid = this.validate();
+        return (
+            <form>
+                <Password 
+                    label="Password" 
+                    value={this.state.password} 
+                    onChange={(e) => {
+                            this.setState({password : e.target.value})
+                        }
+                    } 
+                    valid={valid}
+                    
+                />
+                <Password 
+                    label="Confirm password" 
+                    value={this.state.confirm} 
+                    onChange={(e) => {
+                        this.setState({confirm : e.target.value})
+                        }
+                    }  
+                    valid={valid}
+                />
+            </form>
+        );
+    }
 }
 
 
